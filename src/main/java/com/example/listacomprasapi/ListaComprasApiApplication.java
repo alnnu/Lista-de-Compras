32976b4b946a98ab6f0ea649eb27fd93f93d7563
alnnu <luannf1020@gmail.com>
2024-10-22 17:30:27 -0300

    first commit

---
+++ src/main/java/com/example/listacomprasapi/ListaComprasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.listacomprasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaComprasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaComprasApiApplication.class, args);
+    }
+
+}
